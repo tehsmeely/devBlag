295107988404389ce5ef532f2464333d57a53cb9
Tehsmeely <jon.heiser@gmail.com>
2015-05-09 16:49:28 +0100

    updates to admin for developer model

--- devBlag/admin.py
+++ devBlag/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, Resource, Resource_map, Project
+from .models import Post, Resource, Resource_map, Project, Developer, DevProj_mapping
 
 admin.site.register(Post)
 admin.site.register(Resource_map)
 admin.site.register(Resource)
 admin.site.register(Project)
+admin.site.register(Developer)
+admin.site.register(DevProj_mapping)
